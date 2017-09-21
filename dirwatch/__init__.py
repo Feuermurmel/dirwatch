2c011cc4387c65e7ff8f58e587d2c8ac09ae763c
Michael Schwarz <michi.schwarz@gmail.com>
2017-09-21 23:40:41 +0200

    Factor out state machine

--- dirwatch/__init__.py
+++ dirwatch/__init__.py
@@ -2,10 +2,11 @@
 
 import os
 import sys
+import signal
+from subprocess import Popen, TimeoutExpired
 from argparse import ArgumentParser
 from fnmatch import fnmatchcase
-from subprocess import Popen
-from threading import Event
+from queue import Queue
 
 from os.path import normpath
 from watchdog.events import DirModifiedEvent, FileSystemEventHandler
@@ -21,15 +22,6 @@ def log(msg, *args):
         '{}: {}'.format(os.path.basename(sys.argv[0]), msg.format(*args)),
         file=sys.stderr,
         flush=True)
-
-
-def exec(args):
-    process = Popen(args)
-    process.wait()
-
-    if process.returncode:
-        raise UserError(
-            'Command failed with exit code {}.'.format(process.returncode))
 
 
 def parse_args():
@@ -65,6 +57,13 @@ def parse_args():
              'reports the exist status in any case.')
 
     parser.add_argument(
+        '-k',
+        '--kill',
+        action='store_true',
+        help='Kill and restart the command when it is still running when '
+             'additional changes are detected.')
+
+    parser.add_argument(
         '--debug',
         action='store_true',
         help='Enable debug mode. This will print all events and whether it '
@@ -76,13 +75,69 @@ def parse_args():
     return parser.parse_args()
 
 
-def main(directory, include, exclude, command, watch, debug):
-    if include is None:
-        include = ['*']
+class Manager:
+    def __init__(self, *, command, watch):
+        self._watch = watch
+        self._command = command
 
-    if exclude is None:
-        exclude = ['.*']
+        self._current_process = None
+        self._pending_modification = False
 
+    def handle_modification(self):
+        self._pending_modification = True
+        self._check_pending_modification()
+
+    def handle_sigchld(self):
+        if self._current_process is not None:
+            self._reap_process()
+
+    def handle_exit(self):
+        if self._current_process is not None:
+            self._terminate_process()
+            self._current_process.wait()
+
+    def _check_pending_modification(self):
+        if self._pending_modification:
+            if self._current_process is None:
+                self._pending_modification = False
+                self._start_process()
+
+    def _start_process(self):
+        assert self._current_process is None
+
+        if self._watch:
+            print('\x1bc', end='', flush=True)
+
+        self._current_process = Popen(self._command, start_new_session=True)
+
+    def _reap_process(self):
+        try:
+            self._current_process.wait(0)
+        except TimeoutExpired:
+            pass
+        else:
+            if self._watch:
+                returncode = self._current_process.returncode
+
+                if returncode:
+                    log('Command failed with exit code {}.'.format(returncode))
+                else:
+                    log('Command completed successfully.')
+
+            self._current_process = None
+            self._check_pending_modification()
+
+    def _terminate_process(self):
+        log('Sending SIGTERM to process group.')
+
+        try:
+            os.killpg(self._current_process.pid, signal.SIGTERM)
+        except PermissionError:
+            # Happens when the process has already exited.
+            pass
+
+
+def start_observer(directory, include, exclude, debug, on_modification):
     def path_matches(path):
         # A path must be matched by any include pattern and by none of the
         # exclude patterns.
@@ -102,8 +157,6 @@ def main(directory, include, exclude, command, watch, debug):
                 i is not None and path_matches(normpath(i))
                 for i in paths)
 
-    change_event = Event()
-
     class EventHandler(FileSystemEventHandler):
         def on_any_event(self, event):
             matches = event_matches(event)
@@ -112,29 +165,44 @@ def main(directory, include, exclude, command, watch, debug):
                 log('{}: {}', event, 'Matched' if matches else 'Ignored')
 
             if matches:
-                change_event.set()
+                on_modification()
 
     event_handler = EventHandler()
 
     observer = Observer()
     observer.schedule(event_handler, directory, recursive=True)
+
     observer.start()
 
-    while True:
-        if watch:
-            print('\x1bc', end='', flush=True)
 
-            try:
-                exec(command)
-            except UserError as e:
-                log('{}', e)
-            else:
-                log('Command completed successfully.')
-        else:
-            exec(command)
+def main(directory, include, exclude, command, watch, debug):
+    if include is None:
+        include = ['*']
 
-        change_event.wait()
-        change_event.clear()
+    if exclude is None:
+        exclude = ['.*']
+
+    manager = Manager(command=command, watch=watch)
+    event_queue = Queue()
+
+    signal.signal(signal.SIGCHLD, lambda signal, frame: manager.handle_sigchld())
+    signal.signal(signal.SIGTERM, signal.default_int_handler)
+
+    start_observer(
+        directory,
+        include,
+        exclude,
+        debug,
+        lambda: event_queue.put(manager.handle_modification))
+
+    try:
+        # Start the process once initially, even without any events.
+        manager.handle_modification()
+
+        while True:
+            event_queue.get()()
+    except KeyboardInterrupt:
+        manager.handle_exit()
 
 
 def entry_point():
